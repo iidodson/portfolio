1cfd49a7133fb4b49d2911854b1b4025e41d320d
Indya Dodson <iidodson@yahoo.com>
2020-02-17 16:17:43 -0800

    Updates

---
+++ precache-manifest.a678070173d02dc5fc6b1d47e788f222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc22a3fae9cfa45a5971b6921c80a9d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fad2cc3287ae5315ee20",
+    "url": "/portfolio/static/css/main.8c7bc835.chunk.css"
+  },
+  {
+    "revision": "de715a78bbd04c9bb5aa",
+    "url": "/portfolio/static/js/2.3825b18f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.3825b18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad2cc3287ae5315ee20",
+    "url": "/portfolio/static/js/main.a8d040ef.chunk.js"
+  },
+  {
+    "revision": "d89ad7dd13ac8d86cc0e",
+    "url": "/portfolio/static/js/runtime-main.94c268e8.js"
+  }
+]);
